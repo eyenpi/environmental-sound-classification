0489d36c1f3c2e01b4280d62e1ed7f9159c8ac71
Ali Nabipour <60272711+eyenpi@users.noreply.github.com>
2022-05-19 12:37:49 +0430

    requirements added

--- setup.py
+++ setup.py
@@ -1,4 +1,18 @@
 
 from setuptools import setup, find_packages
 
-setup(name='audio-classifier', version='1.0', packages=find_packages())
+setup(
+    name='audio-classifier',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pandas',
+        'torch',
+        'torchaudio',
+        'torchvision',
+        'pytorch_lightning',
+        'pyaudio',
+        'scikit-learn',
+    ]
+)
