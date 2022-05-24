1d2a73fbdd2c2c347766182f62e7aad1eb600ed5
Ali Nabipour <60272711+eyenpi@users.noreply.github.com>
2022-05-24 15:50:16 +0430

    offline and train test updated

--- tests/offline.py
+++ tests/offline.py
@@ -1,13 +1,14 @@
 import torch
-
 from classifier.model.cnnmodel import CNNModel
 from classifier.utility.wav2spec import wav2spec
 
 model = CNNModel().load_from_checkpoint(
-    "lightning_logs\checkpoints\epoch=2-step=702.ckpt")
+    "lightning_logs\checkpoints\epoch=9-step=2340.ckpt")
+
+model.eval()
 
 spec = wav2spec("lightning_logs\\102871-8-0-10.wav",
-                n_fft=2048, hop_len=512, n_mels=64, sample_rate=16000, chunk_size=64000)
+                n_fft=2048, hop_len=512, n_mels=64, sample_rate=16000, chunk_size=32000)
 output = model(spec)
 _, pred = torch.max(output, dim=1)
 print(pred)
--- tests/train.py
+++ tests/train.py
@@ -4,6 +4,6 @@ from classifier.dataset.dataset import AudioDataModule
 import pytorch_lightning as pl
 
 model = CNNModel()
-datamod = AudioDataModule("./urbansound8k/", n_fft=2048, hop_len=512, n_mels=64, sample_rate=16000, chunk_size=64000)
-trainer = pl.Trainer(gpus=1, max_epochs=3)
+datamod = AudioDataModule("./urbansound8k", n_fft=2048, hop_len=512, n_mels=64, sample_rate=16000, chunk_size=32000)
+trainer = pl.Trainer(gpus=1, max_epochs=10)
 trainer.fit(model, datamodule=datamod)
