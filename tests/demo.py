6956bd85ce8ef281293c0d4d7ecbc67f982ef1bf
Ali Nabipour <60272711+eyenpi@users.noreply.github.com>
2022-05-31 01:26:20 +0430

    bye playsound, hi pydub

--- tests/demo.py
+++ tests/demo.py
@@ -8,21 +8,15 @@ from classifier.utility.wav2spec import wav2spec
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
-from playsound import playsound
 from threading import Thread
+from pydub import AudioSegment
+from pydub.playback import play
 
-def plays():
-    playsound('lightning_logs\long\longlong.wav', block= False)
-
-idtoclass = {0: 'air_conditioner', 1: 'car_horn', 2: 'children_playing', 3: 'dog_bark', 4: 'drilling', 5: 'engine_idling', 6: 'gun_shot', 7: 'jackhammer', 8: 'siren', 9: 'street_music'}
-
-fig, ax = plt.subplots()
-plt.ion()
-plt.show()
+idtoclass = {0: 'air_conditioner', 1: 'car_horn', 2: 'children_playing', 3: 'dog_bark',
+             4: 'drilling', 5: 'engine_idling', 6: 'gun_shot', 7: 'jackhammer', 8: 'siren', 9: 'street_music'}
 
 model = CNNModel().load_from_checkpoint(
     "lightning_logs\checkpoints\\resnetlast.ckpt")
-
 model.eval()
 
 sample_rate = 16000
@@ -31,13 +25,20 @@ buffer_size = 32000
 frame = buffer_size // chunk_size
 
 buffer = torch.zeros([1, buffer_size])
-
 wave, sr = torchaudio.load('lightning_logs\long\longlong.wav')
 audio_mono = torch.mean(wave, dim=0, keepdim=True)
 audio_mono = torchaudio.transforms.Resample(sr, 16000)(audio_mono)
 
-T = Thread(name='sound',target=plays)
-T.start()
+x = [i for i in range(0, 58)]
+y = []
+fig, ax = plt.subplots()
+plt.ion()
+plt.show()
+
+song = AudioSegment.from_wav("lightning_logs\long\longlong.wav")
+t = Thread(target=play, args=(song,))
+t.start()
+
 for i in range(1000):
     buffer = torch.roll(buffer, (frame-1)*chunk_size, dims=1)
     aud = audio_mono[:, i*chunk_size:(i+1)*chunk_size]
@@ -50,10 +51,21 @@ for i in range(1000):
     output = F.softmax(model(spec), dim=1)
     prob, pred = torch.max(output, dim=1)
     if prob > 0.9:
+        lastp = pred
         print(f"prediciton: {pred.item()} probability: {prob.item():.2f}")
         plt.title(idtoclass[pred.item()])
+        y.append(pred.item())
+    else:
+        y.append(lastp.item())
 
-    librosa.display.specshow(spec.detach().reshape(64, -1).numpy(), sr=16000, hop_length=256, ax=ax)
+    librosa.display.specshow(spec.detach().reshape(64, -1).numpy(),
+                             sr=16000, hop_length=256, ax=ax)
     plt.pause(0.005)
-    if i < 32 or i > 38:
+    if i < 32 or i > 42:
         sleep(0.8)
+
+# plt.close()
+# plt.ioff()
+# plt.plot(x, y)
+# plt.yticks(range(10), idtoclass.values())
+# plt.show(block=True)
